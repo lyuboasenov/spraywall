0537b15facd91ce9430f1e68d505694f2e6cbfdf
lyuboasenov <lyubo.asenov@gmail.com>
2024-03-15 09:50:19 +0200

    show template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+    { title: 'Routes', url: '/boulders', icon: 'reorder-four' },
+    { title: 'Add route', url: '/boulders/add', icon: 'add' },
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
