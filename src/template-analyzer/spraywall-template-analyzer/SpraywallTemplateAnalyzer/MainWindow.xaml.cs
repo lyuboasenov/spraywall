dd8eaac607b33aee280cefedcea34a561937e388
lyuboasenov <lyubo.asenov@gmail.com>
2024-03-29 09:26:34 +0200

    select ellipse

--- src/template-analyzer/spraywall-template-analyzer/SpraywallTemplateAnalyzer/MainWindow.xaml.cs
+++ src/template-analyzer/spraywall-template-analyzer/SpraywallTemplateAnalyzer/MainWindow.xaml.cs
@@ -1,37 +1,24 @@
-﻿using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.OCR;
-using Emgu.CV.Structure;
-using Emgu.CV.Util;
+﻿using Emgu.CV.Structure;
 using SkiaSharp;
 using SpraywallTemplateAnalyzer.ImageProcessing;
+using SpraywallTemplateAnalyzer.Models;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static Emgu.Util.Platform;
 
 namespace SpraywallTemplateAnalyzer {
    /// <summary>
    /// Interaction logic for MainWindow.xaml
    /// </summary>
    public partial class MainWindow : Window {
-      private FileInfo sourceImgFileInfo;
+
       private string imgLocation;
       private TemplateProcessor _processor;
       private SKBitmap _bitmap;
+      private List<SelectableRotatedRect> _selectableEllipses = new List<SelectableRotatedRect>();
 
       public MainWindow() {
          InitializeComponent();
@@ -49,7 +36,6 @@ namespace SpraywallTemplateAnalyzer {
 
          if (dialog.ShowDialog() ?? false) {
             imgLocation = dialog.FileName;
-            sourceImgFileInfo = new FileInfo(imgLocation);
             _processor = TemplateProcessor.Process(imgLocation);
             slArea.Value = _processor.MinArea;
             slRatio.Value = _processor.MaxRatio;
@@ -59,9 +45,24 @@ namespace SpraywallTemplateAnalyzer {
             img.Width = _bitmap.Width;
             img.Height = _bitmap.Height;
             img.InvalidateVisual();
+            ReLoadEllipses();
          }
       }
 
+      private void ReLoadEllipses() {
+         lstEllipses.ItemsSource = null;
+         _selectableEllipses.Clear();
+         
+         foreach(var el in _processor?.FilteredEllipses ?? Enumerable.Empty<RotatedRect>()) {
+            var elItem = new SelectableRotatedRect() {
+               RotatedRect = el,
+               IsSelected = false
+            };
+            _selectableEllipses.Add(elItem);
+         }
+
+         lstEllipses.ItemsSource = _selectableEllipses;
+      }
 
       private void img_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e) {
          if (_bitmap != null) {
@@ -72,8 +73,9 @@ namespace SpraywallTemplateAnalyzer {
                paint.Style = SKPaintStyle.Stroke;
 
                canvas.DrawImage(SKImage.FromBitmap(_bitmap), new SKPoint());
-               if (_processor != null && _processor.FilteredEllipses.Any()) {
-                  foreach (var el in _processor.FilteredEllipses) {
+               if (_selectableEllipses.Any()) {
+                  foreach (var elItem in _selectableEllipses.Where(e => e.IsSelected)) {
+                     var el = elItem.RotatedRect;
                      canvas.RotateDegrees(el.Angle, el.Center.X, el.Center.Y);
                      canvas.DrawOval(el.Center.X, el.Center.Y, el.Size.Width / 2, el.Size.Height / 2, paint);
                      canvas.RotateDegrees(-el.Angle, el.Center.X, el.Center.Y);
@@ -87,6 +89,7 @@ namespace SpraywallTemplateAnalyzer {
          if (null != _processor) {
             _processor.MaxSize = (uint) e.NewValue;
             img.InvalidateVisual();
+            ReLoadEllipses();
          }
       }
 
@@ -94,6 +97,7 @@ namespace SpraywallTemplateAnalyzer {
          if (null != _processor) {
             _processor.MinArea = (uint) e.NewValue;
             img.InvalidateVisual();
+            ReLoadEllipses();
          }
       }
 
@@ -101,7 +105,12 @@ namespace SpraywallTemplateAnalyzer {
          if (null != _processor) {
             _processor.MaxRatio = (uint) e.NewValue;
             img.InvalidateVisual();
+            ReLoadEllipses();
          }
+      }
+
+      private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+         img.InvalidateVisual();
       }
    }
 }
---
+++ src/template-analyzer/spraywall-template-analyzer/SpraywallTemplateAnalyzer/Models/SelectableRotatedRect.cs
@@ -0,0 +1,8 @@
+﻿using Emgu.CV.Structure;
+
+namespace SpraywallTemplateAnalyzer.Models {
+   public class SelectableRotatedRect {
+      public RotatedRect RotatedRect { get; set; }
+      public bool IsSelected { get; set; }
+   }
+}
