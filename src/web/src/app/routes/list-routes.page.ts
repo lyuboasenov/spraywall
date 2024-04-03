93f4457deb3644f97148704c015fea4ed32be7d7
Lyuboslav Asenov <lasenov@vmware.com>
2024-04-03 14:09:44 +0300

    css

--- src/web/src/app/routes/list-routes.page.ts
+++ src/web/src/app/routes/list-routes.page.ts
@@ -3,9 +3,9 @@ import { RouteService } from '../services/route.service';
 import { Route } from '../models/route';
 
 @Component({
-  selector: 'app-list-boulders',
-  templateUrl: './list-boulders.page.html',
-  styleUrls: ['./list-boulders.page.scss'],
+  selector: 'app-list-routes',
+  templateUrl: './list-routes.page.html',
+  styleUrls: ['./list-routes.page.scss'],
 })
 export class ListRoutesPage {
   public routes: Route[] = [];
